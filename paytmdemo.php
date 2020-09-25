ed11ac02256c1894b0d234a35eb77555964898a5
saran-90 <67274573+saran-90@users.noreply.github.com>
2020-09-26 00:56:26 +0530

    Add files via upload
    Second commit

---
+++ paytmdemo.php
@@ -0,0 +1,17 @@
+Test Merchant IDwzTNto29769215795602
+
+Test Merchant Keyt@i2dvNOH08PHSHT
+
+WebsiteWEBSTAGING
+
+Industry TypeRetail
+
+Channel ID (For Website)WEB
+
+Channel ID (For Mobile Apps)WAP
+
+Transaction URL
+
+Transaction Status URL
+
+Production
